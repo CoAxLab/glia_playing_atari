5c57de14cd6dd1c5cc149b0136996339bccb6408
Erik Peterson <Erik.Exists@gmail.com>
2018-10-12 11:44:42 -0700

    Draft glia_digits

--- gliafun/exp/glia_digits.py
+++ gliafun/exp/glia_digits.py
@@ -1,43 +1,74 @@
 from __future__ import print_function
-import argparse
+import fire
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
-from gliafun.gn import LocalLinear
+from gliafun import gn
 
 
-class Net(nn.Module):
-    def __init__(self, glia=True, connections=3, overlap=1):
-        super(Net, self).__init__()
+class DigitGlia(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # --------------------------------------------------------------------
+        # Build visual neurons
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
 
-        self.glia = glia
-        if self.glia:
-            self.fc1 = nn.LocalLinear(
-                320, 50, connections=connections, overlap=overlap)
-            self.fc2 = nn.LocalLinear(
-                50, 10, connections=connections, overlap=overlap)
-        else:
-            self.fc1 = nn.Linear(320, 50)
-            self.fc2 = nn.Linear(50, 10)
+        # --------------------------------------------------------------------
+        # Build glia decision layers
+
+        # Shrink from 320 -> 50
+        glia1 = []
+        for s in reversed(range(50, 320, 2)):
+            glia1.extend(gn.GliaShrink(s, bias=False))
+            glia1.extend(torch.nn.Tanh())
+        self.fc1 = nn.Sequential(*glia1)
+
+        # Shrink from 50 -> 10
+        glia2 = []
+        for s in reversed(range(10, 50, 2)):
+            glia2.extend(gn.GliaShrink(s, bias=False))
+            if s > 10:  # Last glia cells should be linear
+                glia2.extend(torch.nn.Tanh())
+        self.fc2 = nn.Sequential(*glia2)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = self.fc1(x)  # nonlin is built in
+        # x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+
+        return F.log_softmax(x, dim=1)
+
+
+class DigitNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        # x = F.dropout(x, training=self.training)
         x = self.fc2(x)
 
         return F.log_softmax(x, dim=1)
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch, log_interval=10):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -46,13 +77,13 @@ def train(args, model, device, train_loader, optimizer, epoch):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
+        if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
 
-def test(args, model, device, test_loader):
+def test(model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
@@ -73,61 +104,19 @@ def test(args, model, device, test_loader):
             100. * correct / len(test_loader.dataset)))
 
 
-def main():
+def main(glia=False,
+         batch_size=64,
+         test_batch_size=1000,
+         epochs=10,
+         lr=0.01,
+         momentum=0.5,
+         use_cuda=False,
+         seed=1,
+         log_interval=10):
+    """Glia learn to see (digits)"""
+    # ------------------------------------------------------------------------
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument(
-        '--batch-size',
-        type=int,
-        default=64,
-        metavar='N',
-        help='input batch size for training (default: 64)')
-    parser.add_argument(
-        '--test-batch-size',
-        type=int,
-        default=1000,
-        metavar='N',
-        help='input batch size for testing (default: 1000)')
-    parser.add_argument(
-        '--epochs',
-        type=int,
-        default=10,
-        metavar='N',
-        help='number of epochs to train (default: 10)')
-    parser.add_argument(
-        '--lr',
-        type=float,
-        default=0.01,
-        metavar='LR',
-        help='learning rate (default: 0.01)')
-    parser.add_argument(
-        '--momentum',
-        type=float,
-        default=0.5,
-        metavar='M',
-        help='SGD momentum (default: 0.5)')
-    parser.add_argument(
-        '--no-cuda',
-        action='store_true',
-        default=False,
-        help='disables CUDA training')
-    parser.add_argument(
-        '--seed',
-        type=int,
-        default=1,
-        metavar='S',
-        help='random seed (default: 1)')
-    parser.add_argument(
-        '--log-interval',
-        type=int,
-        default=10,
-        metavar='N',
-        help='how many batches to wait before logging training status')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
-
-    torch.manual_seed(args.seed)
-
+    torch.manual_seed(seed)
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
@@ -140,7 +129,7 @@ def main():
                 transforms.ToTensor(),
                 transforms.Normalize((0.1307, ), (0.3081, ))
             ])),
-        batch_size=args.batch_size,
+        batch_size=batch_size,
         shuffle=True,
         **kwargs)
     test_loader = torch.utils.data.DataLoader(
@@ -151,18 +140,31 @@ def main():
                 transforms.ToTensor(),
                 transforms.Normalize((0.1307, ), (0.3081, ))
             ])),
-        batch_size=args.test_batch_size,
+        batch_size=test_batch_size,
         shuffle=True,
         **kwargs)
 
-    model = Net().to(device)
-    optimizer = optim.SGD(
-        model.parameters(), lr=args.lr, momentum=args.momentum)
+    # ------------------------------------------------------------------------
+    # Model init
+    if glia:
+        model = DigitGlia().to(device)
+    else:
+        model = DigitNet().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+    # ------------------------------------------------------------------------
+    for epoch in range(1, epochs + 1):
+        train(
+            model,
+            device,
+            train_loader,
+            optimizer,
+            epoch,
+            log_interval=log_interval,
+        )
+        test(model, device, test_loader)
 
 
+# ----------------------------------------------------------------------------
 if __name__ == '__main__':
-    main()
+    fire.Fire(main)
