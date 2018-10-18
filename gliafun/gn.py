628031c5b151347b889f0cc46a8dc3d80cd8719d
Erik Peterson <Erik.Exists@gmail.com>
2018-10-17 17:15:46 -0700

    Fix indexing/math in Grow/Shrink

--- gliafun/gn.py
+++ gliafun/gn.py
@@ -20,20 +20,59 @@ class Base(Module):
             self.bias.data.uniform_(-stdv, stdv)
 
     def extra_repr(self):
-        return 'in_features={}, out_features={}, pad={}, bias={}'.format(
-            self.in_features, self.out_features - (2 * self.pad), self.pad,
-            self.bias is not None)
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None)
 
     def forward(self, input):
         pass
 
 
+# class GliaGrow(Base):
+#     def __init__(self, in_features, bias=True):
+#         self.step = 2  # Actual growth
+#         self.pad = 2  # Projection dummy
+#         out_features = in_features + self.step + (2 * self.pad)
+
+#         super().__init__(in_features, out_features, bias=bias)
+
+#         # Init params
+#         self.weight = Parameter(
+#             torch.Tensor(self.out_features, self.in_features))
+#         if bias:
+#             self.bias = Parameter(torch.Tensor(self.out_features))
+#         else:
+#             self.register_parameter('bias', None)
+#         self.reset_parameters()
+
+#     def forward(self, input):
+#         # Make batch compatible
+#         output = torch.zeros(input.shape[0], self.out_features)
+
+#         # Nearest-neighbor linear transform
+#         i, j = 0, 3
+#         for n in range(self.in_features):
+#             if self.bias is not None:
+#                 output[:, i:j] += F.linear(
+#                     input[:, i:j], self.weight[n, i:j].unsqueeze(1).t(),
+#                     self.bias[i:j])
+#             else:
+#                 output[:, i:j] += F.linear(
+#                     input[:, i:j], self.weight[n, i:j].unsqueeze(1).t(),
+#                     self.bias)
+
+#             # Update index
+#             i += 1
+#             j += 1
+
+#         output = output[:, self.pad:-self.pad]  # Strip pad
+
+#         return output
+
+
 class GliaGrow(Base):
     def __init__(self, in_features, bias=True):
-        self.step = 2  # Actual growth
-        self.pad = 2  # Projection dummy
-        out_features = in_features + self.step + (2 * self.pad)
-
+        # Init out
+        out_features = in_features + 2
         super().__init__(in_features, out_features, bias=bias)
 
         # Init params
@@ -47,31 +86,33 @@ class GliaGrow(Base):
 
     def forward(self, input):
         # Make batch compatible
-        output = torch.zeros(input.shape[0], self.out_features)
+        batch_size = input.shape[0]
+        output = torch.zeros(batch_size,
+                             self.out_features)  #, requires_grad=True)
 
         # Nearest-neighbor linear transform
         i, j = 0, 3
         for n in range(self.in_features):
+            # Calc update
+            update = input[:, n].unsqueeze(1) * self.weight[i:j, n].unsqueeze(
+                1).t()
+
+            # And apply it
+            output[:, i:j] += update
+
             if self.bias is not None:
-                output[:, i:j] += F.linear(input[:, n].unsqueeze(1),
-                                           self.weight[i:j, n].unsqueeze(1),
-                                           self.bias[i:j])
-            else:
-                output[:, i:j] += F.linear(input[:, n].unsqueeze(1),
-                                           self.weight[i:j, n].unsqueeze(1),
-                                           self.bias)
+                output[:, i:j] += self.bias[i:j]
+
             # Update index
             i += 1
             j += 1
-
-        output = output[:, self.pad:-self.pad]  # Strip pad
 
         return output
 
 
 class GliaShrink(Base):
     def __init__(self, in_features, bias=True):
-        self.pad = 0
+        # Init out
         out_features = max(in_features - 2, 1)
         super().__init__(in_features, out_features, bias=bias)
 
@@ -86,19 +127,28 @@ class GliaShrink(Base):
 
     def forward(self, input):
         # Make batch compatible
-        output = torch.zeros(input.shape[0], self.out_features)
+        batch_size = input.shape[0]
+        output = torch.zeros(batch_size,
+                             self.out_features)  #, requires_grad=True)
 
         # Nearest-neighbor linear transform
-        i, j = 0, 3
-        for n in range(self.out_features):
+
+        # Set initial index, handling the the out_features=1
+        # edge case
+        i, j = 0, min(3, self.out_features + 1)
+
+        # Iter over input, calculating running sums on the output
+        # with the i,j index
+        for n in range(max(self.in_features - 2, 1)):
+            # Calc update
+            update = input[:, n].unsqueeze(1) * self.weight[i:j, n].unsqueeze(
+                1).t()
+
+            # And apply it
+            output[:, i:j] += update
+
             if self.bias is not None:
-                output[:, i:j] += F.linear(input[:, n].unsqueeze(1),
-                                           self.weight[i:j, n].unsqueeze(1),
-                                           self.bias[i:j])
-            else:
-                output[:, i:j] += F.linear(input[:, n].unsqueeze(1),
-                                           self.weight[i:j, n].unsqueeze(1),
-                                           self.bias)
+                output[:, i:j] += self.bias[i:j]
 
             # Update index
             i += 1
