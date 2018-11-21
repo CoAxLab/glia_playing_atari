e51de32f0884a9f451b8d986d62893bdb9c75cd5
Erik Peterson <Erik.Exists@gmail.com>
2018-11-21 11:56:53 -0800

    VAE > AGN working/learning (unopt)

--- glia/exp/glia_digits.py
+++ glia/exp/glia_digits.py
@@ -5,99 +5,67 @@ Code modified from:
 https://github.com/pytorch/examples/blob/master/mnist/main.py
 """
 from __future__ import print_function
+import os
 import fire
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from torchvision.utils import save_image
 
 from glia import gn
 from random import shuffle
 
 
-class SkipInputGlia(nn.Module):
-    """A minst digit glai perceptron, w/ 'skipped input'. """
-
-    def __init__(self, skip_features=24, num_skip=1):
-        super().__init__()
-        self.skip_features = skip_features
-        self.num_skip = num_skip
-
-        # --------------------------------------------------------------------
-        # Skip inputs
-        # TODO skips have a huge lin reduction 784 -> ~20. Several of these
-        # along the AGN. Repeated seq sampling to overcome the d reduction
-        # bottleneck. The Net can add in information missed earlier in the
-        # the AGN 'Calcium wave'?
-
-        # fc0: 784 -> skip_features
-        self.fc0 = []
-        for n in range(self.num_skip):
-            if n == 0:
-                self.fc0.append(
-                    nn.Sequential(nn.Linear(784, skip_features), nn.ELU()))
-            else:
-                self.fc0.append(
-                    nn.Sequential(
-                        nn.Linear(784 + skip_features, skip_features),
-                        nn.ELU()))
-
-        # --------------------------------------------------------------------
-        # Following the Perceptrion Net Shape
-        # fc1
-        self.fc1 = nn.Sequential(gn.Slide(skip_features), nn.ELU())
-
-        # --------------------------------------------------------------------
-        # fc2: Linear readout, (skip_features) -> 10
-        glia2 = []
-        for s in reversed(range(10 + 2, skip_features, 2)):
-            glia2.append(gn.Gather(s, bias=False))
-            if s > 12:  # Linear on the last output
-                glia2.append(torch.nn.ELU())
-        self.fc2 = nn.Sequential(*glia2)
-
-    def forward(self, x):
-        # Reshape x and make a frozen copy (called x_in)
-        x = x.view(-1, 784)
-        x_in = x.clone()
-
-        # fc0: skip inputs
-        for n, fc in enumerate(self.fc0):
-            if n == 0:
-                x = fc(x_in)
-            else:
-                x = fc(torch.cat((x_in, x), 1))
-
-        # fc1: nonlin slide
-        x = self.fc1(x)
-
-        # fc2: shrink, then 'linear' decode
-        x = self.fc2(x)
-
-        return F.log_softmax(x, dim=1)
-
-
-class PerceptronGlia(nn.Module):
-    """A minst digit perceptron, made only of glia layers"""
+class VAE(nn.Module):
+    """A MINST VAE."""
 
     def __init__(self):
         super().__init__()
 
-        # This strond d reduction is bio motivated. 'thousands of
-        # neuron contact a single astrocyte (from SFN2018 presentation;
-        # need cite).
-        # fc0: 784 -> 24
-        self.fc0 = nn.Linear(784, 24)
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
 
-        # fc1: 24 -> 24
-        self.fc1 = nn.Sequential(gn.Slide(24), nn.ELU())
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
 
-        # fc2: Linear readout, 256 -> 10
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar, z
+
+
+class PerceptronGlia(nn.Module):
+    """A minst digit perceptron, made only of glia layers.
+    
+    Note: assumes input is from a VAE.
+    """
+
+    def __init__(self, z_features=20):
+        super().__init__()
+        self.z_features = z_features
+
+        # Input comp
+        self.fc1 = nn.Sequential(gn.Slide(self.z_features), nn.ELU())
+
+        # Reaout compression (linear in last)
         glia2 = []
-        for s in reversed(range(10 + 2, 24, 2)):
+        for s in reversed(range(10 + 2, self.z_features, 2)):
             glia2.append(gn.Gather(s, bias=False))
-
             # Linear on the last output
             if s > 12:
                 glia2.append(torch.nn.ELU())
@@ -105,8 +73,6 @@ class PerceptronGlia(nn.Module):
         self.fc2 = nn.Sequential(*glia2)
 
     def forward(self, x):
-        x = x.view(-1, 784)
-        x = F.elu(self.fc0(x))
         x = self.fc1(x)  # ELU implicit
         x = self.fc2(x)  # ELU then Linear @ final layer.
 
@@ -114,79 +80,99 @@ class PerceptronGlia(nn.Module):
 
 
 class PerceptronNet(nn.Module):
-    def __init__(self):
+    """A minst digit perceptron.
+
+    Note: assumes input is from a VAE.
+    """
+
+    def __init__(self, z_features=20):
         super().__init__()
-        self.fc0 = nn.Linear(784, 256)
-        self.fc1 = nn.Linear(256, 256)
-        self.fc2 = nn.Linear(256, 10)
+        self.z_features = z_features
+
+        self.fc1 = nn.Linear(self.z_features, self.z_features)
+        self.fc2 = nn.Linear(self.z_features, 10)
 
     def forward(self, x):
-        x = x.view(-1, 784)
-        x = F.relu(self.fc0(x))
-        x = F.relu(self.fc1(x))
+        x = F.elu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
 
-class ConvGlia(nn.Module):
-    """A minsy digit perceptron."""
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
-    def __init__(self):
-        super().__init__()
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-        # --------------------------------------------------------------------
-        # Build visual neurons
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-
-        # --------------------------------------------------------------------
-        # Build glia decision layers
-
-        # CHEATING FOR TRAINSPEED.
-        self.fc1 = nn.Linear(320, 20)
-
-        # GLIA make the final decision
-        glia1 = []
-        for s in reversed(range(10 + 2, 22, 2)):
-            glia1.append(gn.Gather(s, bias=False))
-
-            # Linear for the last set; matches the DigitNet.
-            if s > 12:
-                glia1.append(torch.nn.ELU())
-
-        self.fc2 = nn.Sequential(*glia1)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.tanh(self.fc1(x))
-        x = self.fc2(x)
-
-        return F.log_softmax(x, dim=1)
+    return BCE + KLD
 
 
-class ConvNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+def train_vae(model,
+              device,
+              train_loader,
+              optimizer,
+              epoch,
+              log_interval=10,
+              debug=False):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar, _ = model(data)
+        loss = loss_function(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+        if debug and (batch_idx % log_interval == 0):
+            print('>>> VAE train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.
+                  format(epoch, batch_idx * len(data),
+                         len(train_loader.dataset),
+                         100. * batch_idx / len(train_loader),
+                         loss.item() / len(data)))
 
-    def forward(self, x):
-        x = F.tanh(F.max_pool2d(self.conv1(x), 2))
-        x = F.tanh(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.tanh(self.fc1(x))
-        x = self.fc2(x)
+    print('====> VAE Epoch: {} Average loss: {:.4f}'.format(
+        epoch, train_loss / len(train_loader.dataset)))
 
-        return F.log_softmax(x, dim=1)
+
+def test_vae(model,
+             device,
+             test_loader,
+             epoch,
+             batch_size,
+             progress=False,
+             debug=False,
+             data_path="."):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for i, (data, _) in enumerate(test_loader):
+            data = data.to(device)
+            recon_batch, mu, logvar, _ = model(data)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
+            if i == 0:
+                n = min(data.size(0), 8)
+                comparison = torch.cat(
+                    [data[:n],
+                     recon_batch.view(batch_size, 1, 28, 28)[:n]])
+                save_image(
+                    comparison.cpu(),
+                    os.path.join(data_path,
+                                 "reconstruction_{}.png".format(epoch)),
+                    nrow=n)
+
+    test_loss /= len(test_loader.dataset)
+    if progress or debug:
+        print('>>> VAE test loss: {:.4f}'.format(test_loss))
+    return test_loss
 
 
 def train(model,
+          model_vae,
           device,
           train_loader,
           optimizer,
@@ -200,8 +186,9 @@ def train(model,
         # Get batch data
         data, target = data.to(device), target.to(device)
 
-        # Get return
-        output = model(data)
+        # Classify
+        _, _, _, z = model_vae(data)
+        output = model(z)
 
         # Learn
         loss = F.nll_loss(output, target)
@@ -219,15 +206,21 @@ def train(model,
                     100. * batch_idx / len(train_loader), loss.item()))
 
 
-def test(model, device, test_loader, progress=False, debug=False):
+def test(model, model_vae, device, test_loader, progress=False, debug=False):
     # Test
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
+            # Get batch data
             data, target = data.to(device), target.to(device)
-            output = model(data)
+
+            # Classify
+            _, _, _, z = model_vae(data)
+            output = model(z)
+
+            # Scores
             test_loss += F.nll_loss(
                 output, target, reduction='sum').item()  # sum up batch loss
             pred = output.max(
@@ -247,20 +240,19 @@ def test(model, device, test_loader, progress=False, debug=False):
 
 
 def main(glia=False,
-         conv=True,
-         skip=False,
-         skip_features=24,
-         num_skip=1,
          batch_size=64,
-         test_batch_size=1000,
+         test_batch_size=128,
          epochs=10,
          lr=0.01,
+         vae_path=None,
+         lr_vae=1e-3,
          use_cuda=False,
          device_num=0,
          seed=1,
          log_interval=50,
          progress=False,
-         debug=False):
+         debug=False,
+         data_path=None):
     """Glia learn to see (digits)"""
     # ------------------------------------------------------------------------
     # Training settings
@@ -270,63 +262,89 @@ def main(glia=False,
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
         torch.cuda.set_device(device_num)
 
+    if data_path is None:
+        data_path = "data"
+
+    # ------------------------------------------------------------------------
+    # Get and pre-process data
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            '../data',
+            data_path,
             train=True,
             download=True,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307, ), (0.3081, ))
-            ])),
+            transform=transforms.ToTensor(),
+        ),
         batch_size=batch_size,
         shuffle=True,
         **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
-            '../data',
+            data_path,
             train=False,
-            transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307, ), (0.3081, ))
-            ])),
+            transform=transforms.ToTensor(),
+        ),
         batch_size=test_batch_size,
         shuffle=True,
         **kwargs)
 
     # ------------------------------------------------------------------------
-    # Model init
-    if glia:
-        if conv:
-            model = ConvGlia().to(device)
-        else:
-            if skip:
-                model = SkipInputGlia(
-                    skip_features=skip_features, num_skip=num_skip).to(device)
-            else:
-                model = PerceptronGlia().to(device)
+    # Decision model
+    # Init
+    # Init
+    if vae_path is None:
+        model_vae = VAE().to(device)
+        optimizer_vae = optim.Adam(model_vae.parameters(), lr=lr_vae)
     else:
-        if conv:
-            model = ConvNet().to(device)
-        else:
-            model = PerceptronNet().to(device)
+        model_vae = None  ## TODO load me
 
-    # optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
+    if glia:
+        model = PerceptronGlia().to(device)
+    else:
+        model = PerceptronNet().to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
-    # ------------------------------------------------------------------------
+    # Learn classes
     for epoch in range(1, epochs + 1):
+        # Learn z?
+        if vae_path is None:
+            train_vae(
+                model_vae,
+                device,
+                train_loader,
+                optimizer_vae,
+                epoch,
+                log_interval=log_interval,
+                debug=debug)
+
+            test_loss = test_vae(
+                model_vae,
+                device,
+                test_loader,
+                epoch,
+                test_batch_size,
+                debug=debug,
+                progress=progress,
+                data_path=data_path)
+
+        # Glia learn
         train(
             model,
+            model_vae,
             device,
             train_loader,
             optimizer,
             epoch,
             log_interval=log_interval,
             debug=debug)
+
         test_loss, correct = test(
-            model, device, test_loader, debug=debug, progress=progress)
+            model,
+            model_vae,
+            device,
+            test_loader,
+            debug=debug,
+            progress=progress)
 
     print(">>> After training:")
     print(">>> Loss: {:.5f}, Correct: {:.2f}".format(test_loss, 100 * correct))
