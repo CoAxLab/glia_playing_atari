1a6414984c499ec23ff7d88fdd3359737c13a935
Erik Peterson <Erik.Exists@gmail.com>
2019-08-23 14:21:12 -0700

    Update tune_digts

--- glia/exp/tune_digits.py
+++ glia/exp/tune_digits.py
@@ -1,183 +1,139 @@
-"""Hyperparameters sweeps. Glia learning digits"""
 import fire
-import torch
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
-
 import ray
-import ray.tune as tune
+import os
+from copy import deepcopy
+from multiprocessing import Pool
+
 import numpy as np
+from glia import exp
+from glia.util import save_checkpoint
+from glia.util import load_checkpoint
 
-from glia import gn
-from glia.exp import glia_digits
-from glia.exp.glia_digits import train
-from glia.exp.glia_digits import test
-from glia.exp.glia_digits import train_vae
-from glia.exp.glia_digits import test_vae
-from glia.exp.glia_digits import VAE
-from glia.exp.glia_digits import VAEGather
-from glia.exp.glia_digits import VAESlide
-from glia.exp.glia_digits import VAESpread
-from glia.exp.glia_digits import LinearGather
+from functools import partial
 
 
-def hyper_run(config, reporter):
-    """Glia learn to see (digits)"""
+def get_best_trial(trial_list, metric):
+    """Retrieve the best trial."""
+    return max(trial_list, key=lambda trial: trial[metric])
 
-    default = dict(
-        model='VAEGather',
-        batch_size=128,
-        test_batch_size=128,
-        epochs=10,
-        lr=0.01,
-        epsilon=1e-8,
-        lr_vae=1e-3,
-        use_cuda=False,
-        seed=42,
-        log_interval=50,
-        progress=False,
-        debug=False,
-        data_path=None,
-        use_cude=True,
-    )
-    default.update(config)
-    config = default
 
-    # ------------------------------------------------------------------------
-    # Training settings
-    torch.manual_seed(config["seed"])
-    device = torch.device("cuda" if config["use_cuda"] else "cpu")
-    if config["use_cuda"]:
-        torch.cuda.manual_seed(config["seed"])
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+def get_sorted_trials(trial_list, metric):
+    return sorted(trial_list, key=lambda trial: trial[metric], reverse=True)
 
-    if config["data_path"] is None:
-        config["data_path"] = "data"
 
-    # ------------------------------------------------------------------------
-    # Get and pre-process data
-    kwargs = {
-        'num_workers': 1,
-        'pin_memory': True
-    } if config["use_cuda"] else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(
-            config["data_path"],
-            train=True,
-            download=True,
-            transform=transforms.ToTensor(),
-        ),
-        batch_size=config["batch_size"],
-        shuffle=True,
-        **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(
-            config['data_path'],
-            train=False,
-            transform=transforms.ToTensor(),
-        ),
-        batch_size=config["test_batch_size"],
-        shuffle=True,
-        **kwargs)
+def get_best_result(trial_list, metric):
+    """Retrieve the last result from the best trial."""
+    return {metric: get_best_trial(trial_list, metric)[metric]}
+
+
+def get_configs(trial_list):
+    """Extract configs"""
+
+    return [trial["config"] for trial in trial_list]
+
+
+def get_metrics(trial_list, metric):
+    """Extract metric"""
+    return [trial[metric] for trial in trial_list]
+
+
+def train(exp_func=None, num_epochs=None, seed_value=None, config=None):
+
+    # Run
+    trial = exp_func(num_epochs=num_epochs, seed_value=seed_value, **config)
+
+    # Save metadata
+    trial.update({
+        "config": config,
+        "num_epochs": num_epochs,
+        "seed_value": seed_value
+    })
+
+    return trial
+
+
+def tune_random(name,
+                exp_name,
+                num_epochs=1000,
+                batch_size=128,
+                test_batch_size=128,
+                glia=False,
+                num_samples=10,
+                num_processes=1,
+                verbose=False,
+                seed_value=None,
+                config=None,
+                **sample_kwargs):
+    """Tune hyperparameters of any bandit experiment."""
+    prng = np.random.RandomState(seed_value)
 
     # ------------------------------------------------------------------------
-    # Model init
-    model_vae = VAE()
-    if config["use_cuda"]:
-        model_vae.cuda()
-    optimizer_vae = optim.Adam(model_vae.parameters(), lr=config["lr_vae"])
+    # Init:
+    # Separate name from path
+    path, name = os.path.split(name)
 
-    Model = getattr(glia_digits, config["model"])
-    model = Model(**config["model_params"])
-    if config["use_cuda"]:
-        model.cuda()
-    optimizer = optim.Adam(
-        model.parameters(), lr=config["lr"], eps=config["epsilon"])
+    # Look up the bandit run function were using in this tuning.
+    exp_func = getattr(exp, exp_name)
+
+    # Build the parallel callback
+    trials = []
+
+    def append_to_results(result):
+        trials.append(result)
+
+    # Setup default params
+    params = dict(
+        exp_func=exp_func,
+        num_epochs=num_epochs,
+        seed_value=seed_value,
+        batch_size=batch_size,
+        test_batch_size=test_batch_size,
+        config={})
 
     # ------------------------------------------------------------------------
-    # Learn digits
-    for epoch in range(1, config["epochs"] + 1):
-        # Learn latent z
-        train_vae(
-            model_vae,
-            device,
-            train_loader,
-            optimizer_vae,
-            epoch,
-            log_interval=config["log_interval"],
-            debug=False)
-        vae_loss = test_vae(
-            model_vae,
-            device,
-            test_loader,
-            epoch,
-            config["test_batch_size"],
-            debug=False,
-            progress=False,
-            data_path=config["data_path"])
+    # Run!
+    # Setup the parallel workers
+    workers = []
+    pool = Pool(processes=num_processes)
+    for _ in range(num_samples):
 
-        # Learn to decide
-        train(
-            model,
-            model_vae,
-            device,
-            train_loader,
-            optimizer,
-            epoch,
-            log_interval=config["log_interval"],
-            debug=False)
-        test_loss, test_correct = test(
-            model, model_vae, device, test_loader, debug=False, progress=False)
+        # Reset param sample for safety
+        if config is None:
+            params["config"] = {}
+        else:
+            params["config"] = config
 
-        reporter(mean_loss=test_loss, mean_accuracy=test_correct)
+        # Make a new sample
+        for k, (low, high) in sample_kwargs.items():
+            params["config"][k] = prng.uniform(low=low, high=high)
 
+        # A worker gets the new sample
+        workers.append(
+            pool.apply_async(
+                train, kwds=deepcopy(params), callback=append_to_results))
 
-def tune_1(data_path,
-           num_samples=50,
-           max_iterations=20,
-           use_cuda=False,
-           num_cpus=8,
-           num_gpus=4):
-    """Get in tune."""
-    ray.init(num_cpus=num_cpus, num_gpus=num_gpus)
+    # Get the worker's result (blocks until complete)
+    for worker in workers:
+        worker.get()
+    pool.close()
+    pool.join()
 
-    experiment_spec = {
-        "tune_1": {
-            "run": hyper_run,
-            "stop": {
-                "mean_accuracy": 0.75,
-                "training_iteration": max_iterations,
-            },
-            "config": {
-                "data_path": data_path,
-                "use_cuda": use_cuda,
-                "lr": lambda spec: np.random.uniform(0.005, .01),
-                # "epsilon": lambda spec: np.random.uniform(1e-8, .1),
-                "model": "VAESpread",
-                "model_params": {
-                    "num_hidden": lambda spec: np.random.randint(1, 10),
-                    "activation_function": "ELU",
-                    # lambda spec: np.random.choice([
-                    # "ELU",
-                    # "ReLU",
-                    # "Tanh", ])
-                }
-            },
-            "trial_resources": {
-                "cpu": 1,
-                "gpu": 0.5  # two models/gpu please
-            },
-            "num_samples": num_samples,
-            "local_dir": data_path,
-            "max_failures": 3
-        }
-    }
-    tune.run_experiments(experiment_spec, verbose=1)
+    # ------------------------------------------------------------------------
+    # Save configs and total_R (full model data is dropped):
+    best = get_best_trial(trials, 'total_R')
 
+    # Best trial config
+    best_config = best["config"]
+    best_config.update(get_best_result(trials, 'total_R'))
+    save_checkpoint(
+        best_config, filename=os.path.join(path, name + "_best.pkl"))
 
-if __name__ == "__main__":
-    fire.Fire({"tune_1": tune_1})
+    # Sort and save the configs of all trials
+    sorted_configs = {}
+    for i, trial in enumerate(get_sorted_trials(trials, 'total_R')):
+        sorted_configs[i] = trial["config"]
+        sorted_configs[i].update({"total_R": trial["total_R"]})
+    save_checkpoint(
+        sorted_configs, filename=os.path.join(path, name + "_sorted.pkl"))
+
+    return best, trials
